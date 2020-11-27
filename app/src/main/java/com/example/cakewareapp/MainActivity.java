f5fc1d9e73adf2b2ed48d7ad0e602dea0ea65a8e
Maviya Shaikh <shaikhmaviya1020@gmail.com>
2020-11-27 22:53:26 +0530

    Linking between login and register

--- app/src/main/java/com/example/cakewareapp/MainActivity.java
+++ app/src/main/java/com/example/cakewareapp/MainActivity.java
@@ -4,16 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button rbtn = findViewById(R.id.registerbtn);
-        rbtn.setOnClickListener(new View.OnClickListener(){
+        button = (Button) findViewById(R.id.registerbtn);
+        button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 openRegisterForm();
